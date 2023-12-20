45eb3f0ff4327fee33abb185376176a6a6b39c46
Anouar Doukkali <Anouar.doukkali@gmail.com>
2023-12-20 10:21:10 +0100

    configure kotest from version catalog. pros: - easy to implement cons: - dependencies should be added in every submodule , which can be redundant.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,18 @@ plugins {
     application
 }
 
-kotlin {
-    jvmToolchain(21)
+dependencies {
+    testImplementation(libs.kotest.runner)
+    testImplementation(libs.kotest.assertion)
+    testImplementation(libs.kotest.properties)
 }
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 application {
